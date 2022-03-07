48ccc555ad83e2896bca011a00c01db8cbc3da61
Ashikur Rahman <ashikurrahman@MacBook-Air.local>
2022-03-07 13:15:33 +0100

    up

--- webapp/controller/MyXML.controller.js
+++ webapp/controller/MyXML.controller.js
@@ -7,12 +7,17 @@ sap.ui.define(
       onInit: function () {
         this.oView = this.getView();
         // create our own reuse class to create modelobject
-        var oModel = Models.createJSONModel();
+        var oModel = Models.createJSONModel('model/mockdata/sample.json');
+        //oModel.setDefaultBindingMode('OneWay');
         //3. Make the model available for the appliction
         sap.ui.getCore().setModel(oModel);
         // this get view is also work in this case but it works in the view lebel. IT COULD NOT WORK application level. that means if we want to use thie model another views we have to write it again. For this reason getCore() use is recommneded. that is for application level and we can acces it from any views without writing again and agan.
 
         // this.getView().setModel(oModel);
+
+        // create second model for JSON type
+        var oModel2 = Models.createJSONModel('model/mockdata/dataset.json');
+        sap.ui.getCore().setModel(oModel2, 'got');
         //4.Binding syntax 3-4
         var oSalary = this.getView().byId('idEmpSal');
         oSalary.bindValue('/empStr/salary');
@@ -20,17 +25,31 @@ sap.ui.define(
         oCurr.bindProperty('value', '/empStr/currency');
       },
       onBtnClick: function () {
-        // this is our controller function
         var oInp = this.getView().byId('idInp');
         alert(oInp.getValue());
       },
       onReload: function () {
+        //1:GET THE MODELOBJECT
+        var oModel = sap.ui.getCore().getModel();
+        // 2 : change the data in the model
+        var oData = oModel.getProperty('/empStr');
+
+        console.log(oData);
+        oModel.setProperty('/empStr/empName', 'Spiderman');
+
         // get the view object from the controller object
         // set the value because there is value property
-        this.oView.byId('idEmpId').setValue('100');
-        this.oView.byId('idEmpName').setValue('Ashik');
-        this.oView.byId('idEmpSal').setValue('9500');
-        this.oView.byId('idCurr').setValue('USD');
+        // this.oView.byId('idEmpId').setValue('100');
+        // this.oView.byId('idEmpName').setValue('Ashik');
+        // this.oView.byId('idEmpSal').setValue('9500');
+        // this.oView.byId('idCurr').setValue('USD');
+      },
+      onShow: function () {
+        //1:GET THE MODELOBJECT
+        var oModel = sap.ui.getCore().getModel();
+        // 2 : change the data in the model
+        var oData = oModel.getProperty('/empStr');
+        console.log(oData);
       },
     });
   }
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -2,12 +2,12 @@ sap.ui.define(['sap/ui/model/json/JSONModel'], function (JSONModel) {
   'use strict';
 
   return {
-    createJSONModel: function () {
+    createJSONModel: function (sFile) {
       //1. create a model object
       var oModel = new JSONModel();
 
       // 2. set or load the model here
-      oModel.loadData('model/mockdata/sample.json');
+      oModel.loadData(sFile);
       return oModel;
     },
     cerateXMLMode: function () {},
